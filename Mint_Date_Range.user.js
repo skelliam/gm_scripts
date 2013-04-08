a4376327f03f522cb67e819062ca4a842c27a5c1
Bill Skellenger <wskellenger@marquette.(none)>
2013-04-07 22:12:44 -0400

    Some final improvements to mint script

--- Mint_Date_Range.user.js
+++ Mint_Date_Range.user.js
@@ -18,6 +18,9 @@
 
 var jStartDate = null;
 var jEndDate = null;
+var clrMintGreen = "#DEEFE9";
+var clrPink = "#F9DEDE";
+var clrWhite = "#FFFFFF";
 
 
 var cssText = 
@@ -33,15 +36,22 @@ var cssText =
   "   padding: 2px;" +
   "}\n\n";
 
-function updateURL() {
-   //whenever a date is changed, validate the dates and update the URL
+function updateURL(startdate, enddate) {
+   //finally we actually update the URL in the browser which may force a reload
    var searchstring = "";
+   searchstring += "?startDate=" + startdate.toString('MM/dd/yyyy');
+   searchstring += "&endDate=" + enddate.toString('MM/dd/yyyy');
+   document.location.search = searchstring;
+}
 
-   startdateval = jStartDate.val();
-   enddateval = jEndDate.val();
+function validateAndUpdate() {
+   //whenever a date is changed, validate the dates and update the URL
 
-   startdate = Date.parse(startdateval);
-   enddate = Date.parse(enddateval);
+   var startdateval = jStartDate.val();
+   var enddateval = jEndDate.val();
+
+   var startdate = Date.parse(startdateval);
+   var enddate = Date.parse(enddateval);
 
    if (    (startdateval == "") 
         || (enddateval == "")
@@ -49,50 +59,60 @@ function updateURL() {
         || (enddateval == null) 
       ) 
    {  /* either date blank */
+      if ((startdateval == "") && (enddateval == "")) {
+         //both dates blank by intention
+         updateURL("", "");
+      }
       return;
    }
    else if (enddate < startdate) {
       alert("End date is before start date.");
+      jEndDate.css("background-color", clrPink);
+      jEndDate.prop("title", "End date is before start date.");
       return;
    }
    else if (startdate > enddate) {
       alert("Start date is after end date.");
+      jStartDate.css("background-color", clrPink);
+      jStartDate.css("title", "Start date is after end date.");
       return;
    }
 
-   //everything good up until now -- let's update the URL
-   searchstring += "?startDate=" + startdate.toString('MM/dd/yyyy');
-   searchstring += "&endDate=" + enddate.toString('MM/dd/yyyy');
-   document.location.search = searchstring;
+   updateURL( startdate.toString('MM/dd/yyyy'), enddate.toString('MM/dd/yyyy') );
 }
 
-function validateDate(jObj) {
+function validateDate(jObj, update) {
+   //pass this function the object to validate,
+   //as well as whether or not to update the main document URL at this time
+
    var date = Date.parse(jObj.val());
 
    if (date != null) {
-      jObj.css("background-color", "#DEEFE9");  //this is the Mint background color :)
+      jObj.css("background-color", clrMintGreen);  //this is the Mint background color :)
       jObj.prop('title', "Date OK: " + date.toString('dddd, MMMM d, yyyy'));
    }
    else {
       if (jObj.val()=="") {
-         jObj.css("background-color", "#FFFFFF");  //this color indicates something wrong with the date
+         jObj.css("background-color", clrWhite);  //this color indicates something wrong with the date
          jObj.prop('title', "Enter a date.");
       } 
       else {
-         jObj.css("background-color", "#F9DEDE");  //this color indicates something wrong with the date
+         jObj.css("background-color", clrPink);  //this color indicates something wrong with the date
          jObj.prop('title', "Invalid date, please fix me.");
       }
    }
 
-   updateURL();  /* update the URL with dates */
+   if (update == true) {
+      validateAndUpdate();  /* update the URL with dates */
+   }
 }
 
 function startDateChanged() {
-   validateDate(jStartDate);
+   validateDate(jStartDate, true);
 }
 
 function endDateChanged() {
-   validateDate(jEndDate);
+   validateDate(jEndDate, true);
 }
 
 
@@ -112,6 +132,30 @@ function insertDatePickers() {
    $(".search-container").append("<b>Start Date: </b>", jStartDate, "<b>End Date: </b>", jEndDate);
 }
 
+function fillDatePickers() {
+   //this function will populate the date pickers with dates from the URL
+   if (location.search.contains('&')) {
+      var things = location.search.split('&');
+   }
+   else {
+      return;
+   }
+
+   for (i=0; i<things.length; i++) {
+      var startdate = things[i].match('startDate=(.*)');
+      var enddate = things[i].match('endDate=(.*)');
+      if (startdate != null) {
+         jStartDate.val(startdate[1]);
+      }
+      else if (enddate != null) {
+         jEndDate.val(enddate[1]);
+      }
+   }
+
+   validateDate(jStartDate, false);
+   validateDate(jEndDate, false);
+}
+
 function main() {
    //cross-browser support added from: http://userscripts.org/groups/51
    if (typeof GM_addStyle == 'undefined') {
@@ -126,6 +170,7 @@ function main() {
 
    GM_addStyle( cssText );
    insertDatePickers();
+   fillDatePickers();
 }
 
 main();
